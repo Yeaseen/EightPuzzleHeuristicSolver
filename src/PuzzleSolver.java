d68f4da697f6f2ecf3235420f02eaca61276a0c4
Yeaseen <yeaseen.arafat96@gmail.com>
2018-06-03 00:45:27 +0600

    init

---
+++ src/PuzzleSolver.java
@@ -0,0 +1,15 @@
+
+
+public class PuzzleSolver {
+
+    public static void main(String[] args) {
+
+        System.out.println(args[0]);
+
+        for (int i = 0; i <args.length ; i++) {
+            System.out.printf(args[i]+" ");
+        }
+
+    }
+
+}
